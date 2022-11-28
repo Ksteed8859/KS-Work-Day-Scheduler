6a899ac228a7a8d52c0102f6218760923006ead2
Kate Steed <sunfiresteed@yahoo.com>
2022-11-27 18:34:21 -0700

    added README.md

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,17 @@ $(function () {
   });
 
 
+  $("#9").children(".description").val(localStorage.getItem("9"));
+  $("#10").children(".description").val(localStorage.getItem("10"));
+  $("#11").children(".description").val(localStorage.getItem("11"));
+  $("#12").children(".description").val(localStorage.getItem("12"));
+  $("#13").children(".description").val(localStorage.getItem("13"));
+  $("#14").children(".description").val(localStorage.getItem("14"));
+  $("#15").children(".description").val(localStorage.getItem("15"));
+  $("#16").children(".description").val(localStorage.getItem("16"));
+  $("#17").children(".description").val(localStorage.getItem("17"));
+
+
   function hourTracker(){
 
     $(".time-block").each(function () {
@@ -27,18 +38,7 @@ $(function () {
     })
   }
 
-
-  $("#9").children(".description").val(localStorage.getItem("9"));
-  $("#10").children(".description").val(localStorage.getItem("10"));
-  $("#11").children(".description").val(localStorage.getItem("11"));
-  $("#12").children(".description").val(localStorage.getItem("12"));
-  $("#13").children(".description").val(localStorage.getItem("13"));
-  $("#14").children(".description").val(localStorage.getItem("14"));
-  $("#15").children(".description").val(localStorage.getItem("15"));
-  $("#16").children(".description").val(localStorage.getItem("16"));
-  $("#17").children(".description").val(localStorage.getItem("17"));
-
-
+  
   function displayTime() {
     var now = dayjs().format('dddd, MMMM D');
     timeDisplayEl.text(now);
---
+++ README.md
@@ -0,0 +1,24 @@
+# Kate Steed Calender Website
+
+## Description
+
+This is our weekly project for Module 5: Third Party API's. The goal of this project was for students to use their new knowledge of third party API's, particularly day.js, to create a work-day calender. Students were provided starter HTML and CSS code, but were expected to add their own Javascript code to add functionality to their website. Some features students added through JS include a button that saves data into local storage, a text box that displays the current date via day.js, and different colored time blocks depending on the current hour. 
+
+## Instaliation
+
+Day.js is needed to run most functions in this website. It is already included in the HTML, so as long as it is not removed, the calender should run as intended.
+
+## Usage
+
+The calender is divided up into time blocks based on a 9-5 workday. To add something to the time block, click and type in the textbox. To save your data, click on the save button. That will ensure that your typed data will still be in the time block if you refresh the page. The calender can be accessed from the following link.
+
+https://ksteed8859.github.io/KS-Work-Day-Scheduler/
+
+![demo-gif](./Assets/05-third-party-apis-homework-demo.gif)
+
+## Credits
+
+Starter code aquired from https://github.com/coding-boot-camp/crispy-octo-meme
+
+## License
+N/A
